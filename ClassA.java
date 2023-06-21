589ce7b6c22feea2a8bcc09b88e5c110dfd451d9
Alexander Castillo <fcastillo@hightech-corp.com>
2023-06-20 20:02:11 -0600

    Agregando hotfix, atrbuto lorem agregado a la clase ClassA

--- ClassA.java
+++ ClassA.java
@@ -1,6 +1,7 @@
 public class ClassA{
     private String foo;
     private String bar;
+    private String loremIpsum = "is simply dummy text of the printing and typesetting industry.";
 
     static void concat(String foo, String bar){
         System.out.println(foo + bar);
